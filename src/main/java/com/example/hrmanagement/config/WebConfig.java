718a7093e53839cd11c16d965fc17f5d20a2458d
waranthon klumkiew <waranthron.k@motiftech.com>
2025-09-08 17:54:27 +0700

    fix: backend CORS & config for Render

--- src/main/java/com/example/hrmanagement/config/WebConfig.java
+++ src/main/java/com/example/hrmanagement/config/WebConfig.java
@@ -7,7 +7,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(
+                .allowedOriginPatterns(
                         "http://localhost:5173",
                         "https://hrmanagement-frontend-oedi.onrender.com"
                 )
